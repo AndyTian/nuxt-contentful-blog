f454b447bc1353248b06404c313ca55e8a12a3a5
tianliang <598318876@qq.com>
2018-08-07 13:15:23 +0800

    all

---
+++ pages/_slug.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-offset-2 is-8">
+          <p class="subtitle is-6">
+              <nuxt-link to="/">Back to blog home</nuxt-link>
+          </p>
+
+          <h1 class="title is-2">
+              {{ post.fields.title }}
+          </h1>
+
+          <div class="content" v-html="$md.render(post.fields.content)">
+              {{ post.fields.content }}
+          </div>
+
+          <hr>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import client from "~/plugins/contentful";
+
+export default {
+  asyncData({ params }) {
+    return client
+      .getEntries({
+        content_type: "post",
+        'fields.slug': params.slug
+      })
+      .then(entries => {
+        return { post: entries.items[0] };
+      })
+      .catch(e => console.log(e));
+  },
+  head() {
+    return {
+      title: this.post.fields.title
+    };
+  }
+};
+</script>
+
+
